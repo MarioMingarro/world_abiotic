8bc588861b55ae41f9f0c9509642aa0d4ac029a5
MarioMingarro <mario_mingarro@hotmail.com>
2023-11-30 16:49:22 +0100

    new

--- tables_raster.R
+++ tables_raster.R
@@ -3,10 +3,8 @@ library(foreign)
 library(raster)
 library(tidyverse)
 library(sf)
-#grid <- read_delim("B:/ALEJANDRA/TABLAS/grid.txt", 
-#                           delim = ";", escape_double = FALSE, 
-#                   col_types = cols(CODE = col_skip()), locale = locale(decimal_mark = ","), 
-#                           trim_ws = TRUE)
+library(writexl)
+library(terra)
 
 grid <- sf::read_sf("B:/ALEJANDRA/grid10km/grid_10km_PORC_LAKES_WGS84_25.shp")
 
@@ -24,12 +22,13 @@ for (i in 1:length(archivos)){#length(archivos)
 }
 write_csv2(kk, "B:/ALEJANDRA/A_RESULTADOS/all_variables_grid_realm.csv" )
 
+# Raster creation
 for (i in 1:length(archivos)){
   kk <- read.dbf(archivos[i])
   colnames(kk) <-  c("FID",  "COUNT", "AREA",  "MEAN")
-  kk <- merge(grid, a, by.x="CODE", by.y="CODE")
-  pp <- select(kk, Lat, Long, REALM)
-  rasterized <- rasterize(pp, terra::rast(resolution = 0.0083, crs = "+proj=longlat +datum=WGS84"), pp$REALM)
+  kk <- merge(grid, SOIL_FAO_GRID, by.x="CODE", by.y="FID_", all.x = TRUE)
+  pp <- select(kk, Lat, Long, DOMSOI)
+  rasterized <- rasterize(pp, terra::rast(resolution = 0.0083, crs = "+proj=longlat +datum=WGS84"), pp$DOMSOI)
   writeRaster(rasterized, paste0("B:/ALEJANDRA/TABLAS/RASTER_2/", arc[i], ".tif"))
 }
 
@@ -49,9 +48,6 @@ mm <- terra::focalPairs(d, w=5, "pearson", na.rm=TRUE)
 
 
 
-library(readr)
-library(tidyverse)
-library(writexl)
 variables <-  read_delim("B:/ALEJANDRA/A_RESULTADOS/all_variables_grid_realm.csv",
                          delim = ";",
                          escape_double = FALSE,
@@ -72,9 +68,55 @@ writexl::write_xlsx(no_data, "B:/ALEJANDRA/A_RESULTADOS/data_missing.xlsx")
 write.csv2(no_data, "B:/ALEJANDRA/A_RESULTADOS/data_missing.csv")
 
 archivos <- list.files("B:/ALEJANDRA/TABLAS/", full.names = T, pattern = "\\.dbf$")
+#REALM
 
-library(foreign)
 a <- read.dbf("B:/ALEJANDRA/TABLAS/REALM_GRID_TABLE.dbf")
 a <- a[,c(5,9)]
 kk <- left_join(variables, a)
 
+rm(a)
+
+
+
+# SOil
+#soil <- sf::read_sf("B:/ALEJANDRA/SOIL_CLASS/DSMW.shp")
+#soil <- left_join(soil, cl, by = c("DOMSOI"="DOMSOI"))
+#st_write(soil, "B:/ALEJANDRA/SOIL_CLASS/FAO_SOIL.shp")
+#st_crs(soil)$proj4string
+
+
+
+SOIL_FAO_GRID <- read.dbf("B:/ALEJANDRA/SOIL_CLASS/soil_FAO_grid.dbf")
+
+cl <- read_csv("B:/ALEJANDRA/SOIL_CLASS/classes_soil.csv")
+cl <- unique(classes_soil$DOMSOI)
+cl <- as.data.frame(sort(cl, decreasing = F))
+cl <- cbind(cl, "FAO_number"=1:nrow(cl))
+colnames(cl) <- c("DOMSOI", "SOIL_CODE")
+rm(classes_soil)
+
+SOIL_FAO_GRID <- left_join(SOIL_FAO_GRID, cl, by = c("MAJORITY" = "SOIL_CODE"))
+SOIL_FAO_GRID <- filter(SOIL_FAO_GRID, SOIL_FAO_GRID$MAJORITY>=1)
+SOIL_FAO_GRID <- as.data.frame(SOIL_FAO_GRID[,c(1,5)])
+colnames(SOIL_FAO_GRID) <- c("FID", "SOIL")
+
+kk <- left_join(grid, SOIL_FAO_GRID, by = c("CODE" = "FID"))
+
+kk <- merge(grid, SOIL_FAO_GRID, by.x="CODE", by.y="FID", all.x = TRUE)
+pp <- select(kk, Lat, Long, SOIL)
+
+#define non-uniform spatial extent
+e.cont = terra::ext(pp[,2,1])
+
+#create raster
+r.cont = terra::rast(e.cont)
+
+rasterized <- terra::rasterize(pp, r.cont, pp$SOIL)
+
+rasterized <- rasterize(pp, terra::rast(resolution = 0.083, crs = "+proj=longlat +datum=WGS84"), pp$SOIL)
+writeRaster(rasterized, paste0("B:/ALEJANDRA/TABLAS/RASTER_2/", arc[i], ".tif"))
+mm <- rast(grid$geometry)
+summary(mm)
+
+variables <- left_join(variables, SOIL_FAO_GRID, by = c("CODE" = "FID_"))
+write.csv2(variables, "B:/ALEJANDRA/A_RESULTADOS/all_variables_grid_realm.csv")
